8c7cc48a65527a2d44ac705aaf077d34723bef8e
miguedubz89 <miguematador8977@gmail.com>
2026-03-16 20:12:07 -0300

    Update server.py

--- server.py
+++ server.py
@@ -231,4 +231,5 @@ if __name__ == '__main__':
     print("  Abrí market-scanner.html en tu browser")
     print("  Ctrl+C para detener")
     print("="*50 + "\n")
-    app.run(host='0.0.0.0', port=5000, debug=False)
+  port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=False)
